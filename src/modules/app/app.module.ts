df006649617e415d8b5dc285a8defa543b9b476b
Prof. Diego <160940877+diegopuc@users.noreply.github.com>
2024-06-21 19:30:49 -0300

    Testes URL servidor web.

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -7,7 +7,7 @@ import { UserModule } from '../user/user.module'
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://jpgomesf:8tMfchBzh24mpcU@uti-processual.2xgklbr.mongodb.net/?retryWrites=true&w=majority',
+      'mongodb+srv://695056:WGZv2cs18ZbE5ee4@mongodb.tr8mlcr.mongodb.net/?retryWrites=true&w=majority&appName=MongoDB',
     ),
     ProcessModule,
     AuthModule,
