0cc942d88501c5916eef3e4b3bcd50acaf3846bf
Prof. Diego <160940877+diegopuc@users.noreply.github.com>
2024-06-24 20:13:39 -0300

    Teste de cors com base https://celke.com.br/artigo/como-permitir-acesso-a-api-com-cors-parte-7

--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,30 @@ const cors = require('cors');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(cors())
+  // app.use(cors())
 
-   app.enableCors({
-     origin: 'https://projetoforum-front.onrender.com', // Adjust according to your needs
-     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-     preflightContinue: false,
-     optionsSuccessStatus: 204,
-     credentials: true,
-     allowedHeaders: [
-       'Accepted',
-       'Content-Type',
-       'Authorization',
-     ]
-   });
+  //  app.enableCors({
+  //    origin: 'https://projetoforum-front.onrender.com', // Adjust according to your needs
+  //    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //    preflightContinue: false,
+  //    optionsSuccessStatus: 204,
+  //    credentials: true,
+  //    allowedHeaders: [
+  //      'Accepted',
+  //      'Content-Type',
+  //      'Authorization',
+  //    ]
+  //  });
+
+  app.use((req, res, next) => {
+    //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+      res.header("Access-Control-Allow-Origin", "*");
+    //Quais são os métodos que a conexão pode realizar na API
+      res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+      app.use(cors());
+      next();
+  });
+
 
   const config = new DocumentBuilder()
     .setTitle('backend-utip')
