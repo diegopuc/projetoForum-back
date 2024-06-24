f854bb5e58d908ee25e6fe33b8659e46f2ed05c0
Prof. Diego <160940877+diegopuc@users.noreply.github.com>
2024-06-24 18:29:57 -0300

    Teste link com https render

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
    app.enableCors({
-     origin: '*', // Adjust according to your needs
+     origin: 'https://projetoforum-front.onrender.com', // Adjust according to your needs
      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
      preflightContinue: false,
      optionsSuccessStatus: 204,
