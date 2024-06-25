9431a8f2b79c265c9beb7da409cd541e3c9746f3
Prof. Diego <160940877+diegopuc@users.noreply.github.com>
2024-06-24 23:42:24 -0300

    Remoção arquivo ampifly

--- src/main.ts
+++ src/main.ts
@@ -5,26 +5,25 @@ import { AppModule } from './modules/app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Use a configuração de CORS do NestJS
-  // app.enableCors({
-  //   origin: 'https://main.d2zml6m6uc2eec.amplifyapp.com', // Ajuste conforme necessário
-  //   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-  //   allowedHeaders: ['Accept', 'Content-Type', 'Authorization', 'X-Requested-With'],
-  //   credentials: true,
-  //   optionsSuccessStatus: 204,
-  // });
+  app.enableCors({
+    origin: 'https://main.d2zml6m6uc2eec.amplifyapp.com', // Ajuste conforme necessário
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Accept', 'Content-Type', 'Authorization', 'X-Requested-With'],
+    credentials: true,
+    optionsSuccessStatus: 204, // Responder com status 204 para requisições OPTIONS
+  });
 
-  // // Adicione middleware personalizado para headers CORS
-  // app.use((req, res, next) => {
-  //   res.header("Access-Control-Allow-Origin", "*");
-  //   res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS");
-  //   res.header("Access-Control-Allow-Headers", "Accept, Content-Type, Authorization, X-Requested-With");
-  //   // Se for uma requisição OPTIONS, responda com 204 No Content
-  //   if (req.method === 'OPTIONS') {
-  //     return res.status(204).end();
-  //   }
-  //   next();
-  // });
+  // Middleware para lidar com as requisições OPTIONS manualmente, se necessário
+  app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+      res.header("Access-Control-Allow-Origin", "https://main.d2zml6m6uc2eec.amplifyapp.com");
+      res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS");
+      res.header("Access-Control-Allow-Headers", "Accept, Content-Type, Authorization, X-Requested-With");
+      res.header("Access-Control-Allow-Credentials", "true");
+      return res.status(204).end();
+    }
+    next();
+  });
 
   const config = new DocumentBuilder()
     .setTitle('backend-utip')
