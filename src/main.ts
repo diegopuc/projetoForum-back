bd33101ca754f84f0659f4c853b257d141630e93
Prof. Diego <160940877+diegopuc@users.noreply.github.com>
2024-06-24 23:04:25 -0300

    Teste de utilizar customhttp.yml amplify

--- src/main.ts
+++ src/main.ts
@@ -6,25 +6,25 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Use a configuração de CORS do NestJS
-  app.enableCors({
-    origin: 'https://main.d2zml6m6uc2eec.amplifyapp.com', // Ajuste conforme necessário
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Accept', 'Content-Type', 'Authorization', 'X-Requested-With'],
-    credentials: true,
-    optionsSuccessStatus: 204,
-  });
+  // app.enableCors({
+  //   origin: 'https://main.d2zml6m6uc2eec.amplifyapp.com', // Ajuste conforme necessário
+  //   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+  //   allowedHeaders: ['Accept', 'Content-Type', 'Authorization', 'X-Requested-With'],
+  //   credentials: true,
+  //   optionsSuccessStatus: 204,
+  // });
 
-  // Adicione middleware personalizado para headers CORS
-  app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Accept, Content-Type, Authorization, X-Requested-With");
-    // Se for uma requisição OPTIONS, responda com 204 No Content
-    if (req.method === 'OPTIONS') {
-      return res.status(204).end();
-    }
-    next();
-  });
+  // // Adicione middleware personalizado para headers CORS
+  // app.use((req, res, next) => {
+  //   res.header("Access-Control-Allow-Origin", "*");
+  //   res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS");
+  //   res.header("Access-Control-Allow-Headers", "Accept, Content-Type, Authorization, X-Requested-With");
+  //   // Se for uma requisição OPTIONS, responda com 204 No Content
+  //   if (req.method === 'OPTIONS') {
+  //     return res.status(204).end();
+  //   }
+  //   next();
+  // });
 
   const config = new DocumentBuilder()
     .setTitle('backend-utip')
