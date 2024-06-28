97747c3d06f621a93bb6cb6eca2080cbba8ee6d9
Fábio Rodrigo de Souza <fabiors@pucpcaldas.br>
2024-06-28 16:36:21 -0300

    Adiciona e configura ServeStaticModule
    Para que seja possível servir os arquivos do frontend a partir do backend

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -3,9 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose'
 import { AuthModule } from '../auth/auth.module'
 import { ProcessModule } from '../process/process.module'
 import { UserModule } from '../user/user.module'
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../..', 'public'), // Adjust the path as per your project structure
+      exclude: ['/api*'], // Exclude API routes
+    }),
     MongooseModule.forRoot(
       'mongodb+srv://695056:WGZv2cs18ZbE5ee4@test.srculmn.mongodb.net/?retryWrites=true&w=majority&appName=test',
       //'mongodb://localhost:27017/local',
